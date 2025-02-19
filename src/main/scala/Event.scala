556254c99b06bcb99ea4616e49279752cb333565
Oiva Ylönen <oiva.ylonen@gmail.com>
2025-02-19 13:48:01 +0200

    Lisätty Event-luokka ja toString-metodi testaukseen

--- src/main/scala/Event.scala
+++ src/main/scala/Event.scala
@@ -1,1 +1,13 @@
-class Event
+package mycalendar
+
+class Event (
+  val name: String,
+  val stertTime: String,
+  val endTime: String,
+  val description: String,
+  val remainder: Boolean,
+  val category: Category
+):
+
+  override def toString: String =
+    s"Event($name, form $stertTime to $endTime, remainder=$remainder, cat=${category.name})"

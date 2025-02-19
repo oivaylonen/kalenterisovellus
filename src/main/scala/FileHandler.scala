57db9eb0fe5dae7a21b8adff11e6ce125f3becdd
Oiva Ylönen <oiva.ylonen@gmail.com>
2025-02-19 16:42:02 +0200

    FileHanler-objekti ja funktio, joka tallentaa .ics muotoon tietoja

---
+++ src/main/scala/FileHandler.scala
@@ -0,0 +1,54 @@
+package mycalendar
+
+import java.io.{File, PrintWriter}
+import scala.io.Source
+import scala.collection.mutable.Buffer
+
+// Filehanler luokka lukee ja kirjoittaa Event-olioita .ics -tiedostoon
+// .ics muoto mahdollistaa kalenterisovellusten yhteensopivuuden
+object FileHandler:
+
+  // Funktio ottaa listan eventtejä ja tallentaa ne haluttuun tiedostoon
+  def saveEventsToIcs(events: Seq[Event], filename: String): Unit =
+
+    // Uusi PrintWrite olio joka kirjoittaa filename tiedostoon
+    val pw = PrintWriter(File(filename))
+
+    // Try-lohkosta löytyy koodi joka kirjoitetaan tiedostoon
+    try
+      // Lisätään aluksi pakolliset otsikkorivit
+      pw.println("BEGIN:VCALENDAR") // Aloitus
+      pw.println("VERSION:2.0") // Versio
+      pw.println("PRODID:-//OivanKalenteri//FI") // Palvelun tarjoaja | Projektin nimi | Lokaatio
+      pw.println("CALSCALE:GREGORIAN") // Kalenterin laskujärjestelmä
+      pw.println("METHOD:PUBLISH") // Jakotapa
+
+      // Käydään kaikki listan Event-oliot läpi, ja muutetaan ne .ics muotoon
+      for e <- events do
+
+        // Lisätään tapahtuman aloitus
+        pw.println("BEGIN:VEVENT")
+
+        // Luodaan uniikki ID tunniste
+        val uid = java.util.UUID.randomUUID().toString
+
+        // Muutetaan start ja end ajat .ics muotoon
+        val icsStart = ICSUtils.toIcsDateTime(e.startTime)
+        val icsEnd = ICSUtils.toIcsDateTime(e.endTime)
+
+        // Lisätään tapahtuman tiedot
+        pw.println(s"UID:$uid")
+        pw.println(s"DTSTART:$icsStart")
+        pw.println(s"DTEND:$icsEnd")
+        pw.println(s"SUMMARY:${e.name}")
+        pw.println(s"DESCRIPTION:${e.description}")
+        pw.println(s"CATEGORIES:${e.category.name}")
+
+        // Lisätään tapahtuman lopetus
+        pw.println("END:VEVENT")
+
+      // Lisätään kalenterin lopetus
+      pw.println("END:VCALENDAR")
+    finally
+      // Suljetaan tiedosto
+      pw.close()

cb2963eef965702d6011806c6b7aed12e835b10e
Oiva Ylönen <oiva.ylonen@gmail.com>
2025-04-05 22:36:44 +0300

    Alustavat napit tapahtumien lisäämiseen ja poistamiseen

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,116 +5,116 @@ import scalafx.scene.Scene
 import scalafx.scene.layout.{BorderPane, HBox, VBox}
 import scalafx.scene.control.Button
 import scalafx.geometry.Pos
-import scalafx.scene.paint.Color
-import views.{WeekView, DayView, MonthView}
+import views.{DayView, WeekView, MonthView, AddEventView, RemoveEventView}
 import java.time.LocalDate
 
 object Main extends JFXApp3 {
 
   override def start(): Unit = {
+
     val rootLayout = new BorderPane()
 
-    val dayViewPane   = DayView.createDayView(LocalDate.now())
-    val weekViewPane  = WeekView.createWeekView(date => {/*TODO: siirry DayViewiin tms.*/})
-    val monthViewPane = MonthView.createMonthView()
+    def buttonStyle =
+      """-fx-background-color: transparent;
+        |-fx-border-color: transparent;
+        |-fx-font-size: 13px;
+        |-fx-cursor: hand;
+        |-fx-padding: 5 10;
+        |-fx-background-radius: 8;
+      """.stripMargin
 
-    // Sivuvalikko (drawer)
+    // Sivuvalikko
     val sideMenu = new VBox {
       spacing = 10
       alignment = Pos.TopLeft
-      prefWidth = 180
       style =
         """-fx-background-color: #f4f4f4;
           |-fx-border-color: #ddd;
           |-fx-padding: 10;
         """.stripMargin
-      visible = false // piilossa oletuksena
 
-      // myöhemmin otetaan käyttöön
       children = Seq(
-        new Button("Lisää tapahtuma") {
-          disable = true
-        },
+        new Button("Lisää tapahtuma")  {
+        style = buttonStyle
+        onAction = _ => {
+        rootLayout.center = AddEventView.createAddEventView()
+        }},
         new Button("Poista tapahtuma") {
-          disable = true
-        },
-        new Button("Filtteröinti") {
-          disable = true
-        },
-        new Button("Haku") {
-          disable = true
-        }
+        style = buttonStyle
+        onAction = _ => {
+        rootLayout.center = RemoveEventView.createRemoveEventView()
+        }},
+        new Button("Filtteröinti")     { disable = true }, //Toteutus tulossa
+        new Button("Haku")            { disable = true } //Toteutus tulossa
       )
     }
-
+    // Sivuvalikko avatessa piilossa
     var sideMenuOpen = false
 
-    // Apufunktio sivuvalikon togglaukseen
     def toggleSideMenu(): Unit = {
       sideMenuOpen = !sideMenuOpen
-      sideMenu.visible = sideMenuOpen
+      if (sideMenuOpen) rootLayout.left = sideMenu
+      else rootLayout.left = null
     }
 
-    // Yläpalkki (sisältää ☰-napin ja päivä/viikko/kuukausi-napit)
+    // Näkymät (päivä, viikko, kuukausi)
+    def showDayView(date: LocalDate): Unit = {
+      rootLayout.center = DayView.createDayView(date)
+    }
+    val weekViewPane = WeekView.createWeekView { clickedDate =>
+      showDayView(clickedDate)
+    }
+    val monthViewPane = MonthView.createMonthView(showDayView)
+
+    // Tästä valitaan aloitusnäkymä
+    rootLayout.center = monthViewPane
+
+    // Yläpalkki (liikkuminen näkymien välillä)
     val hamburgerButton = new Button("☰") {
       onAction = _ => toggleSideMenu()
-      style =
-        """-fx-font-size: 16px;
-          |-fx-background-color: transparent;
-          |-fx-border-color: transparent;
-          |-fx-cursor: hand;
-        """.stripMargin
+      style = buttonStyle
     }
 
-    val dayButton = new Button("Päivä") {
-      onAction = _ => rootLayout.center = dayViewPane
-      style =
-        """-fx-background-color: transparent;
-          |-fx-border-color: transparent;
-          |-fx-font-size: 14px;
-        """.stripMargin
+    val dayButton = new Button("Tänään") {
+      style = buttonStyle
+      onAction = _ => {
+        showDayView(LocalDate.now())
+      }
     }
 
     val weekButton = new Button("Viikko") {
-      onAction = _ => rootLayout.center = weekViewPane
-      style =
-        """-fx-background-color: transparent;
-          |-fx-border-color: transparent;
-          |-fx-font-size: 14px;
-        """.stripMargin
+      style = buttonStyle
+      onAction = _ => {
+        rootLayout.center = weekViewPane
+      }
     }
 
     val monthButton = new Button("Kuukausi") {
-      onAction = _ => rootLayout.center = monthViewPane
-      style =
-        """-fx-background-color: transparent;
-          |-fx-border-color: transparent;
-          |-fx-font-size: 14px;
-        """.stripMargin
+      style = buttonStyle
+      onAction = _ => {
+        rootLayout.center = monthViewPane
+      }
     }
 
     val topBar = new HBox {
       spacing = 20
       alignment = Pos.CenterLeft
       style =
-        """-fx-background-color: #f8f8f8;
+        """-fx-background-color: #fdfdfd;
           |-fx-border-color: #ddd;
-          |-fx-padding: 5 15;
+          |-fx-padding: 10 10;
         """.stripMargin
       children = Seq(hamburgerButton, dayButton, weekButton, monthButton)
     }
 
-    // Aloitusnäkymä
-    rootLayout.left = sideMenu
+    // Yläpalkki näkyviin
     rootLayout.top = topBar
-    rootLayout.center = monthViewPane
 
-    // Pääikkuna
+    // Stage käynnistämissessä
     stage = new JFXApp3.PrimaryStage {
       title = "Kalenterisovellus"
       scene = new Scene {
         root = rootLayout
-        fill = Color.WhiteSmoke
       }
     }
   }
